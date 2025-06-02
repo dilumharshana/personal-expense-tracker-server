d9ac987f12bd36702711d2661b47c8c7e183dbc2
Dilum Harshana <dilum.harshana@itelasoft.com>
2025-06-02 17:20:36 +0530

    fix - fixed cors issues from cloud front

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ import database from "./src/config/db-connection.js";
 // Import api route handler
 import apiRoutes from "./src/routes/index.js";
 
+app.use(
+  cors({
+    origin: "http://15.206.205.178:8080"
+  })
+);
+
 // Initialize express app
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
@@ -25,7 +30,7 @@ app.use(express.json());
 app.use("/api", apiRoutes);
 
 // Start server
-app.listen(PORT, "0.0.0.0", async () => {
+app.listen(PORT, async () => {
   await database.connect();
   console.log(`🚀 Server running on port : http://localhost:${PORT}/`);
 });
